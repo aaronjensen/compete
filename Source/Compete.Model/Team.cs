91aa94cc4f6beaaab2f445f36957d618cc941396
Jeffery Olson <olson.jeffery@gmail.com>
2009-02-15 13:50:43 -0800

    adding some conventions around the team name .. 1-16 chars, word characters only (numbers, letters or underscore)

--- Source/Compete.Model/Team.cs
+++ Source/Compete.Model/Team.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Compete.Model
 {
@@ -9,11 +10,34 @@ namespace Compete.Model
   {
     string _teamName;
     readonly List<TeamMember> _teamMembers;
+    readonly Regex nonWordCharacters = new Regex(@"\W+");
 
     public Team(string teamName)
     {
+      if (!IsValidTeamName(teamName))
+      {
+        throw new ArgumentException("Team name must be less than 16 characters and consist of only numbers and letters");
+      }
       _teamName = teamName;
       _teamMembers = new List<TeamMember>();
+    }
+
+    private bool IsValidTeamName(string name)
+    {
+      if (name.Length > 16)
+      {
+        return false;
+      }
+      if (nonWordCharacters.IsMatch(name))
+      {
+        return false;
+      }
+      if (name.Equals(string.Empty))
+      {
+        return false;
+      }
+      
+      return true;
     }
 
     public void AddTeamMember(TeamMember teamMember)
---
+++ Source/Compete.Specs/Model/TeamSpecs.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Compete.Model;
+using Machine.Specifications;
+
+namespace Compete.Specs.Model
+{
+  public class TeamSpecs
+  {
+    protected static Exception result;
+    protected static Team team;
+    protected static string teamName;
+    protected static string longTeamName = "The Who Cares";
+  }
+
+  public class when_trying_to_create_a_team_with_a_name_that_is_longer_than_sixteen_characters : TeamSpecs
+  {
+    Establish context = () =>
+      teamName = "more than sixteen characters";
+
+    Because of = () =>
+      result = Catch.Exception(()=> new Team(teamName));
+
+    It should_not_allow_the_team_to_be_created = () =>
+      result.GetType().ShouldEqual(typeof(ArgumentException));
+  }
+
+  public class when_trying_to_create_a_team_which_contains_anything_besides_numbers_letters_or_underscores : TeamSpecs
+  {
+    Establish context = () =>
+      teamName = "_%abc 123";
+
+    Because of = () =>
+      result = Catch.Exception(()=> new Team(teamName));
+
+    It should_not_allow_the_team_to_be_created = () =>
+      result.GetType().ShouldEqual(typeof(ArgumentException));
+  }
+
+  public class when_trying_to_create_a_team_with_an_empty_name : TeamSpecs
+  {
+    Establish context = () =>
+      teamName = "";
+
+    Because of = () =>
+      result = Catch.Exception(()=> new Team(teamName));
+
+    It should_not_allow_the_team_to_be_created = () =>
+      result.GetType().ShouldEqual(typeof(ArgumentException));
+  }
+}
