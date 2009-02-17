58a5d8a8a2b35ab0d47067936060f8bc7f82de52
Jeffery Olson <olson.jeffery@gmail.com>
2009-02-17 09:51:35 -0800

    added team summary

---
+++ Source/Compete.Model/Reports/TeamSummary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Compete.Model.Reports
+{
+  public class TeamSummary
+  {
+    public string Name { get; set; }
+    public string LongName { get; set; }
+    public IEnumerable<string> TeamMembers { get; set; }
+
+    public TeamSummary(string teamName, string longName, IEnumerable<string> teamMembers)
+    {
+      Name = teamName;
+      LongName = longName;
+      TeamMembers = teamMembers;
+    }
+  }
+}
