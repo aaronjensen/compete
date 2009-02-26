bef185cf7513f1956af2af73a2d35820d684179c
Jacob Lewallen <jlewallen@gmail.com>
2009-02-26 15:53:03 -0800

    Blank Competition controller and action.
    These will kick off new competitions... and will sooner or later
    only be available to administrators?

---
+++ Source/Compete.Site/Controllers/CompetitionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+using Compete.Site.Filters;
+
+namespace Compete.Site.Controllers
+{
+  [RequireAuthenticationFilter]
+  public class CompetitionController : CompeteController
+  {
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
