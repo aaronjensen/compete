dd3f5c6f28bd52430f0b11a759bc571d42f5d8ea
Aaron Jensen <aaronjensen@gmail.com>
2009-02-26 15:31:11 -0800

    Adding appdomain/assembly collection stuff.

---
+++ Source/Compete.Site/Infrastructure/AppDomainHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Security.Policy;
+
+namespace Compete.Site.Infrastructure
+{
+  public static class AppDomainHelper
+  {
+    public static O InSeparateAppDomain<I, O>(I parameter, Func<I, O> method)
+    {
+      AppDomainSetup setup = new AppDomainSetup();
+      Evidence evidence = AppDomain.CurrentDomain.Evidence;
+      setup.ApplicationBase = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+      AppDomain domain = AppDomain.CreateDomain(Guid.NewGuid().ToString(), evidence, setup);
+      try
+      {
+        Type sandboxType = typeof(Sandbox<I, O>);
+        string sandboxAssemblyName = sandboxType.Assembly.GetName().Name;
+        string sandboxTypeName = sandboxType.FullName;
+        Sandbox<I, O> sandbox = (Sandbox<I, O>)domain.CreateInstanceAndUnwrap(sandboxAssemblyName, sandboxTypeName);
+        O value = sandbox.Apply(parameter, method);
+        return value;
+      }
+      finally
+      {
+        AppDomain.Unload(domain);
+      }
+    }
+  }
+
+  public class Sandbox<I, O> : MarshalByRefObject
+  {
+    static readonly log4net.ILog _log = log4net.LogManager.GetLogger(typeof(AppDomainHelper));
+
+    public O Apply(I parameter, Func<I, O> method)
+    {
+      try
+      {
+        return method(parameter);
+      }
+      catch (Exception error)
+      {
+        _log.Error(error);
+      }
+      return default(O);
+    }
+
+    public override object InitializeLifetimeService()
+    {
+      return null;
+    }
+  }
+}
---
+++ Source/Compete.Site/Infrastructure/AssemblyServiceCollectionFinder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace Compete.Site.Infrastructure
+{
+  public class AssemblyServiceCollectionFinder
+  {
+    readonly List<Assembly> _assemblies = new List<Assembly>();
+    readonly string _rootPath;
+
+    public AssemblyServiceCollectionFinder()
+    {
+      _rootPath = System.IO.Path.GetDirectoryName(typeof(AssemblyServiceCollectionFinder).Assembly.Location);
+    }
+
+    public void AddAllAssemblies(string path)
+    {
+      foreach (string assemblyPath in Directory.GetFiles(path, "*.dll"))
+      {
+        Assembly assembly = Assembly.LoadFrom(assemblyPath);
+        _assemblies.Add(assembly);
+      }
+    }
+
+    public IEnumerable<T> Create<T>()
+    {
+      foreach (Type type in EnumerateTypesOf<T>())
+      {
+        yield return (T)Activator.CreateInstance(type);
+      }
+    }
+
+    private IEnumerable<Type> EnumerateTypesOf<T>()
+    {
+      foreach (Assembly assembly in _assemblies)
+      {
+        foreach (Type type in assembly.GetExportedTypes())
+        {
+          if (typeof(T).IsAssignableFrom(type))
+          {
+            yield return type;
+          }
+        }
+      }
+    }
+  }
+}
