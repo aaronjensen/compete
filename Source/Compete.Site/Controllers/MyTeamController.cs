0bb033cf6384acf69950790dd07b3e1bfe53d62e
Jeffery Olson <olson.jeffery@gmail.com>
2009-02-26 13:04:17 -0800

    file upload impl.. only all .dll files, one 1 at a time.. puts in c:\compete

--- Source/Compete.Site/Controllers/MyTeamController.cs
+++ Source/Compete.Site/Controllers/MyTeamController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,9 +11,40 @@ namespace Compete.Site.Controllers
   [RequireAuthenticationFilter]
   public class MyTeamController : Controller
   {
+    static readonly string _filePath = @"C:\compete";
+
     public ActionResult Index()
     {
       return View();
+    }
+
+    [AcceptVerbs(HttpVerbs.Post)]
+    public ActionResult UploadDll()
+    {
+      if (Request.Files.Count != 1)
+      {
+        throw new FileLoadException("only one file at a time, please. you loaded "+Request.Files.Count+".");
+      }
+      foreach(string file in Request.Files)
+      {
+        var hpf = Request.Files[file];
+
+        if (hpf.ContentLength == 0)
+        {
+          continue;
+        }
+        if (!hpf.FileName.Split('.').Last().ToLower().Equals("dll"))
+        {
+          throw new ArgumentException("only .dll files only, please");
+        }
+
+        string savedFileName = Path.Combine(  
+             _filePath,   
+             Path.GetFileName(hpf.FileName));  
+        
+        hpf.SaveAs(savedFileName);  
+      }
+      return Redirect("~/MyTeam");
     }
   }
 }
