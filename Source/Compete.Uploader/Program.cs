0c49db7f0ecd438b8d240b8dad2b9628dab792ae
Aaron Jensen <aaronjensen@gmail.com>
2009-02-27 14:10:22 -0800

    Making the uploader better

--- Source/Compete.Uploader/Program.cs
+++ Source/Compete.Uploader/Program.cs
@@ -14,17 +14,21 @@ namespace Compete.Uploader
       if (args.Length != 4)
       {
         Console.WriteLine("Usage: Compete.Uploader {url} {dll} {teamName} {password}");
+        Environment.Exit(1);
       }
 
       Console.WriteLine("Uploading...");
       var client = new WebClient();
+      
       try
       {
         client.UploadFile(String.Format("{0}/UploadBot?teamName={1}&password={2}", args[0], args[2], args[3]), "post", args[1]);
-
       }
       catch (Exception err)
       {
+        Console.WriteLine("Coudln't upload, check your username and password");
+        Console.WriteLine("");
+        Console.WriteLine("");
         Console.Error.WriteLine("FAIL: " + err);
         Environment.Exit(1);
       }
