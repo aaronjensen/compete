afeb112f4f3e69b25dbfccf064ea06826026cacb
Jacob Lewallen <jlewallen@gmail.com>
2009-02-27 10:43:23 -0800

    Copying some more sample bots?

--- copybots.cmd
+++ copybots.cmd
@@ -4,5 +4,7 @@ Mkdir C:\Compete\Bots
 Mkdir C:\Compete\Games
 Copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.AlwaysThrows.dll C:\Compete\Bots\player1.dll
 Copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.AlwaysThrows.dll C:\Compete\Bots\player2.dll
+Copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.AlwaysThrows.dll C:\Compete\Bots\player3.dll
+Copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.AlwaysThrows.dll C:\Compete\Bots\player4.dll
 Copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.dll C:\Compete\Games
 Copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.Wrapper\bin\Debug\RockPaperScissorsPro.Wrapper.dll c:\compete\games
