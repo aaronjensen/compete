ce07225b05db9072f34ea9edec94edbf84de8b40
Jacob Lewallen <jlewallen@gmail.com>
2009-02-27 11:50:41 -0800

    Copying a take forever bot.

--- copybots.cmd
+++ copybots.cmd
@@ -6,5 +6,6 @@ Copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Deb
 Copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.AlwaysThrows.dll C:\Compete\Bots\player2.dll
 Copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.AlwaysThrows.dll C:\Compete\Bots\player3.dll
 Copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.AlwaysThrows.dll C:\Compete\Bots\player4.dll
+Copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.AlwaysThrows.dll C:\Compete\Bots\TakeForever.dll
 Copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.dll C:\Compete\Games
 Copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.Wrapper\bin\Debug\RockPaperScissorsPro.Wrapper.dll c:\compete\games
