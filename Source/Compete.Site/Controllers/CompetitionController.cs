bed734adb73e208ef926e23599e7fd9bbcf50516
Jacob Lewallen <jlewallen@gmail.com>
2009-02-26 16:34:51 -0800

    Spiking an implementation of CompetitionFactory.
    This pulls the dynamic assemblies and creates IGame's and IPlayer's from them
    and adds them to the Competition...

--- Source/Compete.Site/Controllers/CompetitionController.cs
+++ Source/Compete.Site/Controllers/CompetitionController.cs
@@ -1,17 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
+using Compete.Model.Game;
 using Compete.Site.Filters;
+using Compete.Site.Infrastructure;
+using Compete.Site.Models;
 
 namespace Compete.Site.Controllers
 {
   [RequireAuthenticationFilter]
   public class CompetitionController : CompeteController
   {
+    readonly CompetitionFactory _competitionFactory = new CompetitionFactory(new AssemblyFileRepository());
+
     public ActionResult Index()
     {
+      Competition competition = _competitionFactory.CreateCompetition();
+      competition.PlayRound();
       return View();
+    }
+  }
+
+  public class CompetitionFactory
+  {
+    readonly AssemblyFileRepository _assemblyFileRepository;
+
+    public CompetitionFactory(AssemblyFileRepository assemblyFileRepository)
+    {
+      _assemblyFileRepository = assemblyFileRepository;
+    }
+
+    public Competition CreateCompetition()
+    {
+      DynamicAssemblyTypeFinder dynamicAssemblyTypeFinder = new DynamicAssemblyTypeFinder();
+      dynamicAssemblyTypeFinder.AddAll(_assemblyFileRepository.FindAllGames());
+      dynamicAssemblyTypeFinder.AddAll(_assemblyFileRepository.FindAllPlayers());
+      IGame game = dynamicAssemblyTypeFinder.Create<IGame>().Single();
+      Competition competition = new Competition(game);
+      foreach (IPlayerFactory playerFactory in dynamicAssemblyTypeFinder.Create<IPlayerFactory>())
+      {
+        competition.AddPlayer(playerFactory.CreatePlayer());
+      }
+      return competition;
     }
   }
 }
--- Source/Compete.Site/Infrastructure/DynamicAssemblyTypeFinder.cs
+++ Source/Compete.Site/Infrastructure/DynamicAssemblyTypeFinder.cs
@@ -2,16 +2,26 @@
 using System.Collections.Generic;
 using System.Reflection;
 
+using Compete.Site.Models;
+
 namespace Compete.Site.Infrastructure
 {
   public class DynamicAssemblyTypeFinder
   {
     readonly List<Assembly> _assemblies = new List<Assembly>();
 
-    public void AddAssembly(string assemblyPath)
+    public void AddAssembly(AssemblyFile assemblyFile)
     {
-      Assembly assembly = Assembly.LoadFrom(assemblyPath);
+      Assembly assembly = Assembly.LoadFrom(assemblyFile.Path);
       _assemblies.Add(assembly);
+    }
+
+    public void AddAll(ICollection<AssemblyFile> files)
+    {
+      foreach (AssemblyFile file in files)
+      {
+        AddAssembly(file);
+      }
     }
 
     public IEnumerable<T> Create<T>()
