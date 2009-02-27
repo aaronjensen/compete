445e9ee7b5dce98f239298c79dc2dc6eef101c78
Aaron Jensen <aaronjensen@gmail.com>
2009-02-26 17:57:08 -0800

    Adding something to copy bots

---
+++ copybots.cmd
@@ -0,0 +1,5 @@
+mkdir c:\compete
+mkdir c:\compete\games
+copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.AlwaysThrows.dll c:\compete\player1.dll
+copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.AlwaysThrows.dll c:\compete\player2.dll
+copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.dll c:\compete\games
