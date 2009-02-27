633d13a1468207d395ddb9d72678e2ef6d837e51
Jacob Lewallen <jlewallen@gmail.com>
2009-02-27 09:38:27 -0800

    Copying the wrapper to Compete and Compete/Games

--- copybots.cmd
+++ copybots.cmd
@@ -4,3 +4,4 @@ copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Deb
 copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.AlwaysThrows.dll c:\compete\player2.dll
 copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.dll c:\compete\games
 copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.Wrapper\bin\Debug\RockPaperScissorsPro.Wrapper.dll c:\compete\games
+copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.Wrapper\bin\Debug\RockPaperScissorsPro.Wrapper.dll c:\compete
