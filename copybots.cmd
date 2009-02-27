a7ab8ea5231ad0c3e2a9203a9df81c072706ddfe
Aaron Jensen <aaronjensen@gmail.com>
2009-02-26 23:16:22 -0800

    Adding wrapper to copybots

--- copybots.cmd
+++ copybots.cmd
@@ -3,3 +3,4 @@ mkdir c:\compete\games
 copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.AlwaysThrows.dll c:\compete\player1.dll
 copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.AlwaysThrows.dll c:\compete\player2.dll
 copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.dll c:\compete\games
+copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.Wrapper\bin\Debug\RockPaperScissorsPro.Wrapper.dll c:\compete\games
