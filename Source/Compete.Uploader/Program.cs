e1b2791fa130a692924a150a375ca8130e81af93
Aaron Jensen <aaronjensen@gmail.com>
2009-02-28 14:11:51 -0800

    Fixing a typo

--- Source/Compete.Uploader/Program.cs
+++ Source/Compete.Uploader/Program.cs
@@ -26,7 +26,7 @@ namespace Compete.Uploader
       }
       catch (Exception err)
       {
-        Console.WriteLine("Couldn't upload, check your username and password");
+        Console.WriteLine("Couldn't upload, uploads may be disabled. Also, check your username and password");
         Console.WriteLine("");
         Console.WriteLine("");
         Console.Error.WriteLine("FAIL: " + err);
