691956aa4e2aa699652c9d20daf03e3a6f720467
Jeffery Olson <olson.jeffery@gmail.com>
2009-02-28 14:14:33 -0800

    impl of specs for the MyTeamController.Index() action

---
+++ Source/Compete.Specs/Controllers/MyTeamControllerSpecs.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Compete.Core.Infrastructure;
+using Compete.Model;
+using Compete.Model.Game;
+using Compete.Model.Repositories;
+using Compete.Site.Controllers;
+using Machine.Specifications;
+using Rhino.Mocks;
+
+namespace Compete.Specs.Controllers
+{
+  public class MyTeamControllerSpecs
+  {
+    protected static ITeamRepository teamRepository;
+    protected static ILeaderboardRepository leaderboardRepository;
+    protected static IFormsAuthentication formsAuthentication;
+    protected static IConfigurationRepository configurationRepository;
+
+    Establish context = () =>
+    {
+      
+    };
+
+    protected static void SetUserAsBeingSignedInWithUsername(string name)
+    {
+      formsAuthentication = MockRepository.GenerateMock<IFormsAuthentication>();
+      formsAuthentication.Stub(x => formsAuthentication.IsCurrentlySignedIn).Return(true);
+      formsAuthentication.Stub(x => formsAuthentication.SignedInUserName).Return(name);
+    }
+
+    protected static void SetupTeamInRepositoryNamed(string name)
+    {
+      var team = new Team(name, name, new TeamMember[0], name);
+      teamRepository = MockRepository.GenerateMock<ITeamRepository>();
+      teamRepository.Stub(x => x.FindByTeamName(name)).Return(team);
+    }
+
+    protected static void SetupMatchResultsForTeam(string name)
+    {
+      var leaderboard = new Leaderboard();
+      var matchResults = new[] { MatchResult.WinnerAndLoser(name, Guid.NewGuid().ToString(), "Win") };
+      leaderboard.Include(matchResults);
+
+      leaderboardRepository = MockRepository.GenerateMock<ILeaderboardRepository>();
+      leaderboardRepository.Stub(x => x.GetLeaderboard()).Return(leaderboard);
+    }
+
+    protected static void SetupConfigurationToShowAStartingRoundOf(int round)
+    {
+      configurationRepository = MockRepository.GenerateMock<IConfigurationRepository>();
+      var config = new Configuration();
+      round = round == 0 ? 1 : round;
+      while (config.RoundNumber != round)
+      {
+        config.AdvanceToNextRound();
+      }
+      configurationRepository.Stub(x => x.GetConfiguration()).Return(config);
+    }
+  }
+
+  public class when_viewing_the_my_team_page : MyTeamControllerSpecs
+  {
+    static string teamName = "foo";
+    static MyTeamController controller;
+    static int round = 1;
+
+    Establish context = () =>
+    {
+      SetUserAsBeingSignedInWithUsername(teamName);
+      SetupTeamInRepositoryNamed(teamName);
+      SetupMatchResultsForTeam(teamName);
+      SetupMatchResultsForTeam(teamName);
+      SetupConfigurationToShowAStartingRoundOf(round);
+
+      controller = new MyTeamController(teamRepository, leaderboardRepository, formsAuthentication, configurationRepository);
+    };
+
+    Because of = () =>
+      controller.Index();
+
+    It should_should_show_my_team_information = () =>
+      ((Team)controller.ViewData["currentTeam"]).Name.ShouldEqual(teamName);
+
+    It should_provide_the_current_round = () =>
+      ((int)controller.ViewData["currentRound"]).ShouldEqual(1);
+
+    It should_provide_match_results_for_my_team = () =>
+      ((IEnumerable<RecentMatch>)controller.ViewData["results"]).First().Result.ShouldEqual("Win");
+  }
+}
