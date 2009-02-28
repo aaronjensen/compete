c4a75aae1189b83647b4e55dc3caaa888ffd21d5
Aaron Jensen <aaronjensen@gmail.com>
2009-02-28 15:47:54 -0800

    Changing build.cmd

--- build.cmd
+++ build.cmd
@@ -1,5 +1,1 @@
 msbuild Source\Compete.sln
-
-copy Source\Compete.Bot\bin\Debug\Compete.Bot.dll ..\rock_paper_scissors_pro\PackageSource\Libraries
-copy Source\Compete.Uploader\bin\Debug\Compete.Uploader.exe ..\rock_paper_scissors_pro\PackageSource\Tools
-copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro\bin\Debug\RockPaperScissorsPro.dll ..\rock_paper_scissors_pro\PackageSource\Libraries
