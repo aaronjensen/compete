08e9304fb34a7eba4c88b630f51570ebee83f8be
Aaron Jensen <aaronjensen@gmail.com>
2009-02-28 16:01:54 -0800

    Ready for comp

--- copybots.cmd
+++ copybots.cmd
@@ -4,10 +4,10 @@ del /s /q C:\Compete\Games\*
 Mkdir C:\Compete
 Mkdir C:\Compete\Bots
 Mkdir C:\Compete\Games
-Copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.AlwaysThrows.dll C:\Compete\Bots\jacob.dll
-Copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.AlwaysThrows.dll C:\Compete\Bots\dan.dll
-Copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.AlwaysThrows.dll C:\Compete\Bots\nathan.dll
-Copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.AlwaysThrows.dll C:\Compete\Bots\khalil.dll
+rem Copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.AlwaysThrows.dll C:\Compete\Bots\jacob.dll
+rem Copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.AlwaysThrows.dll C:\Compete\Bots\dan.dll
+rem Copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.AlwaysThrows.dll C:\Compete\Bots\nathan.dll
+rem Copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.AlwaysThrows.dll C:\Compete\Bots\khalil.dll
 rem Copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.AlwaysThrows.dll C:\Compete\Bots\TakeForever.dll
 Copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.AlwaysThrows\bin\Debug\RockPaperScissorsPro.dll C:\Compete\Games
 Copy ..\rock_paper_scissors_pro\Source\RockPaperScissorsPro.Wrapper\bin\Debug\RockPaperScissorsPro.Wrapper.dll c:\compete\games
